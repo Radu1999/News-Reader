b0c0277fd2e6e8b282dd36d019b613120b0b25e9
Radu1999 <37249331+Radu1999@users.noreply.github.com>
2021-11-27 15:52:55 +0200

    Functional image binder

--- app/src/main/java/com/radu/newsreader/bindings/ImageBinding.java
+++ app/src/main/java/com/radu/newsreader/bindings/ImageBinding.java
@@ -1,6 +1,5 @@
 package com.radu.newsreader.bindings;
 
-import android.net.Uri;
 import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
@@ -13,10 +12,8 @@ public class ImageBinding {
 
     @BindingAdapter({"imageUrl"})
     public static void loadIMageWithUrl(ImageView imageView, @Nullable String imageUrl) {
-        Glide.with(imageView.getContext()).load(Uri.parse(imageUrl))
-                .centerCrop()
+        Glide.with(imageView.getContext()).load(imageUrl)
                 .placeholder(R.mipmap.ic_launcher)
-                .dontAnimate()
                 .into(imageView);
     }
 }
